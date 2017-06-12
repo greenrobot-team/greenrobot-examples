47f53296e80ed1ff0b804028506f2327902a5448
greenrobot Team <opensource+team@greenrobot>
2017-06-12 13:48:43 +0200

    Make SimpleEntity#id var.

--- objectbox-kotlin/app/src/main/java/com/example/objectbox/entities/SimpleEntity.kt
+++ objectbox-kotlin/app/src/main/java/com/example/objectbox/entities/SimpleEntity.kt
@@ -5,7 +5,7 @@ import io.objectbox.annotation.Id
 
 @Entity
 data class SimpleEntity(
-        @Id val _id: Long?,
+        @Id var _id: Long?,
         val simpleShort: Short?,
         val simpleInt: Int?,
         val simpleLong: Long?
