d304a6fd734cc50b550f8dc90f9c77e0f63a0bab
greenrobot Team <opensource+team@greenrobot>
2017-09-26 08:28:12 +0200

    greendao: Add ToOne to Order entity.

--- greendao/app/src/main/java/com/example/greendao/model/Order.java
+++ greendao/app/src/main/java/com/example/greendao/model/Order.java
@@ -1,19 +1,34 @@
 package com.example.greendao.model;
 
+import org.greenrobot.greendao.DaoException;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.ToOne;
 
 import java.util.Date;
 
 @Entity
 public class Order {
 
-    @Id private Long id;
+    @Id
+    private Long id;
 
     private Date date;
 
     private long customerId;
+
+    @ToOne(joinProperty = "customerId")
+    Customer customer;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 949219203)
+    private transient OrderDao myDao;
 
     @Generated(hash = 171035120)
     public Order(Long id, Date date, long customerId) {
@@ -49,4 +64,85 @@ public class Order {
     public void setCustomerId(long customerId) {
         this.customerId = customerId;
     }
+
+    @Generated(hash = 8592637)
+    private transient Long customer__resolvedKey;
+
+    /** To-one relationship, resolved on first access. */
+    @Generated(hash = 941511332)
+    public Customer getCustomer() {
+        long __key = this.customerId;
+        if (customer__resolvedKey == null || !customer__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            CustomerDao targetDao = daoSession.getCustomerDao();
+            Customer customerNew = targetDao.load(__key);
+            synchronized (this) {
+                customer = customerNew;
+                customer__resolvedKey = __key;
+            }
+        }
+        return customer;
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 625323961)
+    public void setCustomer(@NotNull Customer customer) {
+        if (customer == null) {
+            throw new DaoException(
+                    "To-one property 'customerId' has not-null constraint; cannot set to-one to null");
+        }
+        synchronized (this) {
+            this.customer = customer;
+            customerId = customer.getId();
+            customer__resolvedKey = customerId;
+        }
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 965731666)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getOrderDao() : null;
+    }
+
+
 }
