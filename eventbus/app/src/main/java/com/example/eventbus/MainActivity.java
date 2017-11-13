b51785eeccb507a9e810cac006152954e8b3efb6
greenrobot Team <opensource+team@greenrobot>
2017-11-13 15:21:48 +0100

    eventbus: use experimental observer to register/unregister.

--- eventbus/app/src/main/java/com/example/eventbus/MainActivity.java
+++ eventbus/app/src/main/java/com/example/eventbus/MainActivity.java
@@ -26,18 +26,8 @@ public class MainActivity extends AppCompatActivity {
             ft.add(R.id.containerFragment, new BlankFragment());
             ft.commit();
         }
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
+        EventBus.getDefault().observe(this);
     }
 
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN_ORDERED)
